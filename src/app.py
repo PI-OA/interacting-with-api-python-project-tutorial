70f4cc9838f622468b04a17dd2e06787d0ba74b6
Pablo Oyarzún A <69913334+PI-OA@users.noreply.github.com>
2025-10-06 21:38:29 +0000

    clase final con conclusión

--- src/app.py
+++ src/app.py
@@ -1,10 +1,42 @@
 import os
 import pandas as pd
-import matplotlib.pyplot as plt
+import seaborn as sns
 from dotenv import load_dotenv
 
 # load the .env file variables
 load_dotenv()
 
 
-# Spotify API credentials
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+print("CLIENT_ID:", client_id)
+print("CLIENT_SECRET:", client_secret)
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+#2YZyLoL8N0Wb9xBt1NhZWg
+
+
+
+# obtener mejores canciones del artista
+results = spotify.artist_top_tracks("2YZyLoL8N0Wb9xBt1NhZWg")
+print("results:", results)  # Depuración: ver toda la respuesta
+print("Cantidad de tracks:", len(results.get('tracks', [])))  # Depuración: ver cuántos tracks hay
+
+
+songs = []
+
+for track in results['tracks']:
+    songs.append({"name":track['name'],
+                 "popularity":track ['popularity'],
+                 "duracion_min":track['duration_ms']/60000})
+    
+print(songs)
+
+df = pd.DataFrame(songs)
+print(df)
